a00cdd100381fabdc00ec326e591d0c1d631d9b4
Rostislav <nehaiki@ukr.net>
2021-10-18 00:41:22 +0300

    modal-add

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,33 @@
 $('.control__title').on('click', function () {
     $(this).next().slideToggle(300);
 });
+
+
+function closedClick(wrap, item) {
+
+    let wrapper = $(wrap),
+        element = $(item);
+
+    $(document).mouseup(function (e) {
+        if (!wrapper.is(e.target) && wrapper.has(e.target).length === 0) {
+            element.slideUp(300);
+            element.removeClass('active');
+        }
+    });
+}
+
+closedClick('.control__title', '.control__list');
+
+
+$('.custum-check__input').on('click', function () { $(this).toggleClass('active'); });
+
+
+$('.currencies__btn-add').on('click', function () {
+    $('.currencies__wrapper').css('display', 'none');
+    $('.modal-add').css('display', 'block');
+});
+
+$('.modal-add__closed').on('click', function () {
+    $('.modal-add').css('display', 'none');
+    $('.currencies__wrapper').css('display', 'block');
+});
