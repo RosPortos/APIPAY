ff25bbd1e80448d2ee34981634da82358e76219d
dreamnoob <vitaliklio1100@gmail.com>
2021-10-17 16:00:52 +0300

    add personal btn

---
+++ src/js/components/profile.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    $('.profile-main__btn').on('click', function () {
+        $('.profile__content').toggleClass('active');
+    });
+
+});
