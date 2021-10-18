82fa43ef00e52d60f46bfad15b0de1bb17f0a0b7
Rostislav <nehaiki@ukr.net>
2021-10-19 01:25:56 +0300

    add admin-direction

--- src/js/global.js
+++ src/js/global.js
@@ -23,7 +23,7 @@ closedClick('.profile-main__btn', '.profile__content');
 $('.custum-check__input').on('click', function () { $(this).toggleClass('active'); });
 
 
-$('.currencies__btn-add').on('click', function () {
+$('.btn-add').on('click', function () {
     $('.currencies__wrapper').css('display', 'none');
     $('.modal-add').css('display', 'block');
 });
@@ -32,3 +32,31 @@ $('.modal-add__closed').on('click', function () {
     $('.modal-add').css('display', 'none');
     $('.currencies__wrapper').css('display', 'block');
 });
+
+const selectTop = $('.select__top'),
+    selectContent = $('.select__content'),
+    selectInput = $(".select__input:not(.select__input-two)"),
+    selectInputCard = $('.select__input-two');
+
+function select() {
+    selectTop.on('click', function () {
+        $(this).toggleClass('active');
+        $(this).parent().toggleClass('active');
+        $(this).next(selectContent).slideToggle(300);
+    });
+
+    selectInput.on('click', function () {
+        let thisText = $(this).find('span').text();
+        let thisParent = $(this).parent();
+        let thisParentPrev = $(this).parent().prev();
+        thisParent.slideUp(300);
+        thisParentPrev.text(thisText).removeClass('active');
+    });
+
+    function hideSelect() {
+        selectContent.slideUp(300);
+        selectTop.removeClass('active');
+        selectTop.parent().removeClass('active');
+    }
+}
+select();
