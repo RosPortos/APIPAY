43296a2c81d2e10d5e8b3e807f1c828028063426
dreamnoob <vitaliklio1100@gmail.com>
2021-10-25 20:58:43 +0300

    add changes

--- src/js/components/toggle.js
+++ src/js/components/toggle.js
@@ -20,8 +20,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     toggleSlideItem('.profile-main__btn', '.profile__content', 300);
-    toggleFadeItem('.sample-popup__close', '.sample-popup', 400);
-    toggleFadeItem('.sample__btn-change', '.sample-popup', 400);
+    toggleFadeItem('.sample-popup__close-change', '.sample-popup-change', 400);
+    toggleFadeItem('.sample__btn-change', '.sample-popup-change', 400);
 
     toggleFadeItem('.main__btn--logo', '.popup--registration', 400);
     toggleFadeItem('.main__btn--header', '.popup--authorization', 400);
@@ -29,4 +29,16 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleFadeItem('.popup__close--authorization', '.popup--authorization', 400);
     toggleFadeItem('.popup__link-registration', '.popup--registration', 400);
     closeFadeItem('.popup__link-registration', '.popup--authorization', 400);
+    toggleFadeItem('.popup__link-password', '.popup--password', 400);
+    closeFadeItem('.popup__link-password', '.popup--authorization', 400);
+    toggleFadeItem('.popup__close--password', '.popup--password', 400);
+    toggleFadeItem('.sample__btn-big', '.sample-popup-add', 400);
+    toggleFadeItem('.sample-popup__close-add', '.sample-popup-add', 400);
+
+    $('.admin-table__nine-btn').on('click', function () {
+        $(this).siblings(".gear__content").fadeToggle(300);
+    });
+    $('.admin-table__nine-btn').on('click', function () {
+        $(this).toggleClass('active');
+    });
 });
--- src/js/global.js
+++ src/js/global.js
@@ -23,19 +23,13 @@ function closedClick(wrap, item, method) {
     });
 }
 
-closedClick('.control__title', '.control__list', slideUp);
+// closedClick('.control__title', '.control__list', slideUp);
 closedClick('.header-profile', '.profile__content', slideUp);
 closedClick('.sample-popup__inner', '.sample-popup', fadeOut);
 closedClick('.popup', '.popup', fadeOut);
 
 $('.custum-check__input').on('click', function () { $(this).toggleClass('active'); });
 
-
-$('.btn-open, .sample__btn-big').on('click', function () {
-    $('.currencies__wrapper, .output__middle').css('display', 'none');
-    $('.modal-add').css('display', 'block');
-    $('.overley').fadeIn(400);
-});
 
 $('.modal-add__closed, .overley').on('click', function () {
     $('.modal-add').css('display', 'none');
